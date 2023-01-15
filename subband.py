13d707fabfa367cf483fe4b206d2f921e53d5948
Dimitris Mylonas <dimmylon@hotmail.com>
2023-01-15 19:00:44 +0200

    Added z frequency plot

--- subband.py
+++ subband.py
@@ -15,13 +15,26 @@ G = make_mp3_synthesisfb(h, M)
 vals = np.zeros(H.shape)
 
 for i in np.arange(H.shape[1]):
-    freq, Hf = signal.freqz(H[:,i])
+    freq, Hf = signal.freqz(H[:,i],fs=fs)
     Hfabs = np.absolute(Hf)
     vals[:,i] = 10 *  np.log10(Hfabs*Hfabs)
 
+z = 13*np.arctan(0.00076*freq) + 3.5*np.arctan((freq/7500)**2)
 
-plt.plot(freq*fs/(2*np.pi),vals)
+
+plt.figure()
+plt.plot(freq,vals)
 plt.xlabel("Hz")
 plt.ylabel("dB")
-plt.title("Μέτρο των συναρτήσεων μεταφοράς των φίλτρων")
+plt.title("Μέτρο των συναρτήσεων μεταφοράς των φίλτρων στη συχνοτητα f")
+
+plt.figure()
+plt.plot(z,vals)
+plt.xlabel("Barks")
+plt.ylabel("dB")
+plt.title("Μέτρο των συναρτήσεων μεταφοράς των φίλτρων στη συχνοτητα ")
 plt.show()
+
+
+
+
