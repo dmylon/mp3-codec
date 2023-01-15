d9a5f15b43dc6604b363152e2242dd483aee4767
Dimitris Mylonas <dimmylon@hotmail.com>
2023-01-15 19:49:54 +0200

    Started codec0

--- subband.py
+++ subband.py
@@ -3,37 +3,55 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from mp3 import make_mp3_analysisfb
 from mp3 import make_mp3_synthesisfb
+from frame import frame_sub_analysis,frame_sub_synthesis
+import wave
 
+
+def plot_frequency(H,fs):
+
+    vals = np.zeros(H.shape)
+
+    for i in np.arange(H.shape[1]):
+        freq, Hf = signal.freqz(H[:,i],fs=fs)
+        Hfabs = np.absolute(Hf)
+        vals[:,i] = 10 *  np.log10(Hfabs*Hfabs)
+
+    plt.figure()
+    plt.plot(freq,vals)
+    plt.xlabel("Hz")
+    plt.ylabel("dB")
+    plt.title("Μέτρο των συναρτήσεων μεταφοράς των φίλτρων στη συχνοτητα f")
+
+    z = 13*np.arctan(0.00076*freq) + 3.5*np.arctan((freq/7500)**2)
+    
+    plt.figure()
+    plt.plot(z,vals)
+    plt.xlabel("Barks")
+    plt.ylabel("dB")
+    plt.title("Μέτρο των συναρτήσεων μεταφοράς των φίλτρων στη συχνοτητα z")
+    plt.show()
+
+
+def codec0(wavin, h, M, N):
+    frame = wavin.readframes(M*N) 
+    H = make_mp3_analysisfb(h, M)
+    Y = frame_sub_analysis(frame,H,20)
+
+
+
+# calling the function
 data = np.load("h.npy", allow_pickle=True).tolist()
 h = data['h'].squeeze()
 M = 32
 fs = 44100
-
 H = make_mp3_analysisfb(h, M)
 G = make_mp3_synthesisfb(h, M)
+#plot_frequency(H,fs)
 
-vals = np.zeros(H.shape)
+N = 36
+wavin = wave.open('myfile.wav', 'r')
+codec0(wavin,h,M,N)
 
-for i in np.arange(H.shape[1]):
-    freq, Hf = signal.freqz(H[:,i],fs=fs)
-    Hfabs = np.absolute(Hf)
-    vals[:,i] = 10 *  np.log10(Hfabs*Hfabs)
-
-z = 13*np.arctan(0.00076*freq) + 3.5*np.arctan((freq/7500)**2)
-
-
-plt.figure()
-plt.plot(freq,vals)
-plt.xlabel("Hz")
-plt.ylabel("dB")
-plt.title("Μέτρο των συναρτήσεων μεταφοράς των φίλτρων στη συχνοτητα f")
-
-plt.figure()
-plt.plot(z,vals)
-plt.xlabel("Barks")
-plt.ylabel("dB")
-plt.title("Μέτρο των συναρτήσεων μεταφοράς των φίλτρων στη συχνοτητα ")
-plt.show()
 
 
 
