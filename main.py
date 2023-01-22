266b5789132700f84ece336a272d14502fd3f0a4
Tasos Tsalakopoulos <47971021+tsalak@users.noreply.github.com>
2023-01-22 22:14:09 +0200

    Added MaskPower, Hz2Barks, STreduction

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from psychoacoustic import *
 
 # calling the function
 data = np.load("h.npy", allow_pickle=True).tolist()
+Tq = np.load("Tq.npy", allow_pickle=True).squeeze()
 h = data['h'].squeeze()
 M = 32
 fs = 44100
@@ -36,4 +37,7 @@ c = frameDCT(Ytot[0:N,:])
 Yh = iframeDCT(c,N,M)
 P = DCTpower(c)
 D = Dksparse(M*N-1)
-ST = STinit(c,D)
+ST = STinit(c,D).astype(np.int32)
+PM = MaskPower(c,ST)
+STr, PMr = STreduction(ST, c, Tq)
+
