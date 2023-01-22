1d635fe08b4eb0f8f534c3e054eba13f55d19f1e
Tasos Tsalakopoulos <47971021+tsalak@users.noreply.github.com>
2023-01-22 22:11:41 +0200

    Added MaskPower, Hz2Barks, STreduction

--- psychoacoustic.py
+++ psychoacoustic.py
@@ -40,3 +40,36 @@ def STinit(c, D):
         
     return ST
 
+
+def MaskPower(c, ST):
+  
+  P  = DCTpower(c)
+  PM = 10*np.log10(10**(0.1*(P[ST])) + 10**(0.1*(P[ST-1])) + 10**(0.1*(P[ST+1])))
+  return PM
+
+
+def Hz2Barks(f):
+  
+  z = 13*np.arctan(0.00076*f) + 3.5*np.arctan((f/7500)**2)
+  return z
+
+
+def STreduction(ST, c, Tq):
+  
+  fs = 44100
+  MN = c.shape[0]
+  PM = MaskPower(c, ST)
+
+  ind = np.where(PM >= Tq[ST])
+  STr, PMr = ST[ind], PM[ind]
+  f = ST*fs/(2*MN)
+  z = Hz2Barks(f)
+
+  for i in np.arange(z.shape[0]-1):
+    if np.absolute(z[i] - z[i+1]) < 0.5:
+      r = i if PM[i] < PM[i+1] else i+1
+      ind = np.where(PM[r]==PMr)[0]
+      STr = np.delete(STr, ind)
+      PMr = np.delete(PMr, ind)
+  return STr, PMr
+
