1793eb716d94997f5299cdb3bb69eedcf0bd5532
tsalak <tsalakop@ece.auth.gr>
2023-02-17 16:04:08 +0200

    rushed huffman implementation

--- huffman.py
+++ huffman.py
@@ -1,13 +1,54 @@
 import numpy as np
 
+def get_huff_symbols(tree):
+
+    left = tree[0]
+    right = tree[1]
+
+    if type(left) is np.ndarray:
+        left_symbols = np.array([[left, ""]],dtype=object)
+    else: 
+        left_symbols = get_huff_symbols(left)
+    if type(right) is np.ndarray:
+        right_symbols = np.array([[right, ""]],dtype=object)
+    else:
+        right_symbols = get_huff_symbols(right)
+
+    left_symbols[:,1] = "0" + left_symbols[:,1]
+    right_symbols[:,1] = "1" + right_symbols[:,1]
+    huff_symbols = np.r_[left_symbols,right_symbols]
+
+
+    return huff_symbols
+
+
 def huff(run_symbols):
 
     unique_sumbols, counts = np.unique(run_symbols, return_counts=True, axis=0)
     prob = counts/np.sum(counts)
     frame_symbol_prob = np.c_[unique_sumbols, prob]
-    frame_symbol_prob = frame_symbol_prob[frame_symbol_prob[:,2].argsort()]
+    order = frame_symbol_prob[:,2].argsort()
     
-    frame_stream = None
+    N = np.array([(frame_symbol_prob[i,:2],frame_symbol_prob[i,2]) for i in order], dtype=object)
+
+    while len(N) > 1:
+        s1, p1 = N[0]
+        s2, p2 = N[1]
+        N = N[2:]
+        new_s = (s1,s2)
+        new_p = p1+p2
+        N = np.r_[N,np.array([(new_s, new_p)], dtype=object)]
+        # probs = np.array([n[1] for n in N])
+        N = N[N[:,1].argsort()]
+
+    huff_symbols = get_huff_symbols(N[0][0])
+
+    frame_stream = ""
+    s = np.array([i for i in huff_symbols[:,0]]).astype(np.int64)
+    for symbol in run_symbols:
+        idx = np.where((s == symbol).all(axis=1))[0][0]
+        frame_stream = frame_stream + huff_symbols[idx,1]
+    print(frame_stream)
     return frame_stream, frame_symbol_prob
 
         
